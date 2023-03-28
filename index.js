5debe85373ea92af9d13a09f05060f444541e1d8
Samoilenko Oleksandr <klaper1993@gmail.com>
2023-03-28 09:21:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Введіть перше число:");
+let num2 = prompt("Введіть друге число:");
+let num3 = prompt("Введіть третє число:");
+
+let average = (num1 + num2 + num3) / 3;
+
+alert(`Середнє арифметичне: ${average}`);
